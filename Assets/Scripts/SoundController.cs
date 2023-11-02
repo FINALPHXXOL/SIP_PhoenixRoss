8686fd9a68e794518a3ca15942d2b120e37ba89e
Phxxol <118862390+Phxxol@users.noreply.github.com>
2023-11-01 19:26:57 -0700

    BIG Progress. What's next is to add more modularity/customization.

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -7,85 +7,73 @@ using static UnityEngine.GraphicsBuffer;
 
 public class SoundController : MonoBehaviour
 {
-    public AudioSource audioSource;
-    public Image image;
-    public AudioMixerGroup mixerGroup; // Reference to the custom AudioMixerGroup
-    public Vector3 offset;
-    public float minOpacity = 0.2f;
-    public float maxOpacity = 1.0f;
-    public float maxDistance;
-    public float rmsMultiplier = 50f;
-    public float maxSize = 200f; // Maximum size the image can reach when the AudioSource is very close
-    public float minSize = 100f; // Minimum size the image will have when the AudioSource is far away
-    public float sizeExponent = 2.0f;
-
-    private Color originalColor;
+    public AudioSettings[] audioSettings;
+    public Canvas canvas;
 
     private void Start()
     {
-        if (audioSource == null)
+        foreach (var settings in audioSettings)
         {
-            Debug.LogError("AudioSource is not assigned!");
-            return;
+            settings.mixerGroup = settings.audioSource.outputAudioMixerGroup;
+            settings.maxDistance = settings.audioSource.maxDistance;
+            settings.originalColor = settings.image.color;
+            if (settings.audioSource != null && settings.audioSource.clip != null)
+            {
+                settings.name = settings.audioSource.clip.name;
+            }
+
+            if (settings.audioSource == null)
+            {
+                Debug.LogError("AudioSource is not assigned for: " + settings.name);
+                continue;
+            }
+
+            if (settings.image == null)
+            {
+                Debug.LogError("Image is not assigned for: " + settings.name);
+                continue;
+            }
+
+            if (settings.mixerGroup == null)
+            {
+                Debug.LogError("AudioMixerGroup is not assigned for: " + settings.name);
+                continue;
+            }
+
+            settings.activeImage = Instantiate(settings.image, canvas.transform);
         }
-
-        if (image == null)
-        {
-            Debug.LogError("Image is not assigned!");
-            return;
-        }
-
-        if (mixerGroup == null)
-        {
-            Debug.LogError("AudioMixerGroup is not assigned!");
-            return;
-        }
-
-        // Assign the custom mixer group to the AudioSource
-        audioSource.outputAudioMixerGroup = mixerGroup;
-        maxDistance = audioSource.maxDistance;
-
-        originalColor = image.color;
     }
 
     private void Update()
     {
-        CalculateOpacity();
-        CalculateImageSizeBasedOnDistance();
-        WaypointIndicator();
-    }
-    private AudioListener FindNearestAudioListener()
-    {
-        AudioListener[] listeners = FindObjectsOfType<AudioListener>();
-        if (listeners.Length == 0)
+        foreach (var settings in audioSettings)
         {
-            Debug.LogWarning("No AudioListener found in the scene.");
-            return null;
-        }
-
-        // Unity allows only one active AudioListener, so just return the first one in the list
-        return listeners[0];
-    }
-    private void CalculateImageSizeBasedOnDistance()
-    {
-        // Find the nearest AudioListener and calculate the distance between AudioSource and it
-        AudioListener nearestListener = FindNearestAudioListener();
-        if (nearestListener != null)
-        {
-            float distance = Vector3.Distance(audioSource.transform.position, nearestListener.transform.position);
-            Debug.Log("Distance: " + distance);
-
-            // Map distance to image size range
-            float imageSize = Mathf.Lerp(maxSize, minSize, 1 - Mathf.InverseLerp(maxDistance, 0f, distance));
-            Debug.Log("Calculated Image Size: " + imageSize);
-
-            image.rectTransform.sizeDelta = new Vector2(imageSize, imageSize);
+            AudioListener nearestListener = FindNearestAudioListener();
+            float distance = Vector3.Distance(settings.audioSource.transform.position, nearestListener.transform.position);
+            bool isWithinMaxDistance = distance < settings.maxDistance;
+            if (settings.activeImage != null && !isWithinMaxDistance)
+            {
+                // Deactivate the image
+                Destroy(settings.activeImage.gameObject);
+                settings.imageIsActive = false;
+            }
+            else if (settings.activeImage == null && isWithinMaxDistance)
+            {
+                // Instantiate the image prefab
+                settings.activeImage = Instantiate(settings.image, canvas.transform);
+                settings.imageIsActive = true;
+            }
+            if (settings.audioSource != null || settings.activeImage != null)
+            {
+                CalculateOpacity(settings);
+                CalculateImageSizeBasedOnDistance(settings);
+                WaypointIndicator(settings);
+            }
         }
     }
 
-    private void CalculateOpacity()
+    private void CalculateOpacity(AudioSettings settings)
     {
-        #region decibels
         float[] samples = new float[256];
         AudioListener.GetOutputData(samples, 1); // Use channel 1 (1-based index) for the custom mixer group
 
@@ -96,29 +84,39 @@ public class SoundController : MonoBehaviour
         }
 
         float rms = Mathf.Sqrt(sum / samples.Length);
-        float decibels = 20f * Mathf.Log10(rms * rmsMultiplier);
+        float decibels = 20f * Mathf.Log10(rms * settings.rmsMultiplier);
 
-        // Map decibels to opacity range
-        float opacity = Mathf.Lerp(minOpacity, maxOpacity, Mathf.InverseLerp(-80f, 0f, decibels));
-        Color newColor = new Color(originalColor.r, originalColor.g, originalColor.b, opacity);
-        image.color = newColor;
-        #endregion decibels
+        float opacity = Mathf.Lerp(settings.minOpacity, settings.maxOpacity, Mathf.InverseLerp(-80f, 0f, decibels));
+        Color newColor = new Color(settings.originalColor.r, settings.originalColor.g, settings.originalColor.b, opacity);
+        settings.activeImage.color = newColor;
     }
 
-    private void WaypointIndicator()
+    private void CalculateImageSizeBasedOnDistance(AudioSettings settings)
     {
-        
-        float minX = image.GetPixelAdjustedRect().width / 2;
-        float maxX = Screen.width - minX;
+        AudioListener nearestListener = FindNearestAudioListener();
+        if (nearestListener != null)
+        {
+            float distance = Vector3.Distance(settings.audioSource.transform.position, nearestListener.transform.position);
 
-        float minY = image.GetPixelAdjustedRect().height / 2;
+            float imageSize = Mathf.Lerp(settings.maxSize, settings.minSize, 1 - Mathf.InverseLerp(settings.maxDistance, 0f, distance));
+            settings.activeImage.rectTransform.sizeDelta = new Vector2(imageSize, imageSize);
+        }
+    }
+
+    private void WaypointIndicator(AudioSettings settings)
+    {
+        float minX = settings.activeImage.GetPixelAdjustedRect().width / 2;
+        float maxX = Screen.width - minX;
+        float minY = settings.activeImage.GetPixelAdjustedRect().height / 2;
         float maxY = Screen.height - minY;
 
-        Vector2 pos = Camera.main.WorldToScreenPoint(audioSource.transform.position + offset);
+        Vector2 pos = Camera.main.WorldToScreenPoint(settings.audioSource.transform.position + settings.offset);
 
-        if(Vector3.Dot((audioSource.transform.position - transform.position), transform.forward) < 0)
+        if (Vector3.Dot((settings.audioSource.transform.position - Camera.main.transform.position), Camera.main.transform.TransformDirection(Vector3.forward)) < 0) 
         {
-            // Target is behind the player
+            print(settings.name + " " + pos.x);
+            print("Screen Width" + Screen.width);
+            //Source is behind player
             if (pos.x < Screen.width / 2)
             {
                 pos.x = maxX;
@@ -129,10 +127,44 @@ public class SoundController : MonoBehaviour
             }
         }
 
+        settings.test = Vector3.Dot((settings.audioSource.transform.position - Camera.main.transform.position), Camera.main.transform.InverseTransformDirection(Vector3.forward));
+
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
 
-        image.transform.position = pos;
-        
+        settings.activeImage.transform.position = pos;
     }
+
+    private AudioListener FindNearestAudioListener()
+    {
+        AudioListener[] listeners = FindObjectsOfType<AudioListener>();
+        if (listeners.Length == 0)
+        {
+            Debug.LogWarning("No AudioListener found in the scene.");
+            return null;
+        }
+
+        return listeners[0];
+    }
+}
+
+[System.Serializable]
+public class AudioSettings
+{
+    public string name; // Name for your reference
+    public AudioSource audioSource;
+    public Image image;
+    public AudioMixerGroup mixerGroup;
+    public Vector3 offset;
+    public float test;
+    public float minOpacity = 0.2f;
+    public float maxOpacity = 1.0f;
+    public float maxDistance;
+    public float rmsMultiplier = 50f;
+    public float maxSize = 200f;
+    public float minSize = 100f;
+    [HideInInspector]
+    public Color originalColor;
+    public bool imageIsActive;
+    public Image activeImage;
 }
